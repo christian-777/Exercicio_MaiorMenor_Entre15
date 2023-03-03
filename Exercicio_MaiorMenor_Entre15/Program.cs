3227b14bc94320aac6f6aa9027c1a845f5d6f9fa
christian-777 <christian.c.g.f777@hotmail.com>
2023-03-03 14:05:29 -0300

    Add project files.

---
+++ Exercicio_MaiorMenor_Entre15/Program.cs
@@ -0,0 +1,35 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //exercicio do maior e do menor numero entre 15 outros
+
+        Random r = new Random();
+        int num, maior=0, menor=0;
+
+        for(int i = 0; i < 15; i++)
+        {
+            num= r.Next(100);
+            Console.WriteLine(num);
+            if(i == 0)
+            {
+                maior = num;
+                menor=num;
+            }
+            else
+            {
+                if (num > maior)
+                {
+                    maior=num;
+
+                }
+                if(num<menor)
+                {
+                    menor=num;
+                }
+            }
+
+        }
+        Console.WriteLine("maior: "+maior+"\nmenor: "+menor);
+    }
+}
